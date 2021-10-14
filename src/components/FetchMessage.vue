9172ebe474c8bf36facc4555f9aca301c090a387
Kazuyuki Miyake <k-miyake@zengeeks.io>
2021-10-14 18:00:34 +0900

    add fetch data component

---
+++ src/components/FetchMessage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Fetch API</h1>
+    <p>
+      <button @click="fetchData">Fetch Data</button>
+    </p>
+    <h3>{{ message }}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    let message = ref('未取得')
+
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:7071/api/probe?name=HackAzure!')
+      const data = await response.json();
+      message.value = data.title
+    }
+
+    return {
+      fetchData,
+      message,
+    }
+  },
+})
+</script>
